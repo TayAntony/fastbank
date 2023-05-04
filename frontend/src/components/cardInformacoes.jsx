f48978b5cf062c20ff159e09b5e856397db3ce30
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-05-04 15:07:59 -0300

    card desenvolvedores trocado para emprestimos

--- frontend/src/components/cardInformacoes.jsx
+++ frontend/src/components/cardInformacoes.jsx
@@ -3,7 +3,7 @@ function CardInformacoes() {
     <>
     <div className=" bg-gradient-to-r from-[#B455FF] to-[#FF5A5A] xs:flex xs:flex-row p-8 justify-around items-center rounded-xl xs:mx-24 flex flex-col gap-1">
         <div className=" flex flex-col items-center">
-            <h1 className="xs:text-4xl text-xl">+ 200mil</h1>
+            <h1 className="xs:text-4xl text-xl">+200mil</h1>
             <p className="underline decoration-solid">usuários</p>
         </div>
         <div className="flex flex-col items-center ">
@@ -16,7 +16,7 @@ function CardInformacoes() {
         </div>
         <div className="flex flex-col items-center">
             <h1 className="xs:text-4xl text-xl">+10mil</h1>
-            <p className="underline decoration-solid">desenvolvedores</p>
+            <p className="underline decoration-solid">empréstimos mensais</p>
         </div>
     </div>
     </> );
