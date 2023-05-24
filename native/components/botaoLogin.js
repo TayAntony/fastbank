6b6a750c03a51600823c276e481d537c3516ac12
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-05-24 16:52:03 -0300

    cor do botão de login desabilitado aplicada junto com o props de validação

--- native/components/botaoLogin.js
+++ native/components/botaoLogin.js
@@ -1,30 +1,27 @@
 import { StyleSheet } from 'react-native';
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View } from "react-native";
 
 export default function BotaoLogin(props) {
     return (
         
-        <View style={styles.botao}>
-            <Text style={{ color: '#FFF', fontSize: 20, alignItems: 'center'}}>
+        <View style={{
+            opacity: 1,
+            height: 60,
+            width: 240,
+            marginTop: 20,
+            borderStyle: 'solid',
+            borderColor: props.validacao ? "white" : "#353535",
+            borderRadius: 12,
+            borderWidth: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: "black",
+            padding: 12}}>
+            <Text style={{ 
+                color: props.validacao ? "white" : "#353535", 
+                fontSize: 20, alignItems: 'center'}}>
                 {props.texto}
             </Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    botao: {
-        opacity: 1,
-        height: 60,
-        width: 240,
-        marginTop: 20,
-        borderStyle: 'solid',
-        borderColor: 'white',
-        borderRadius: 12,
-        borderWidth: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'black',
-        padding: 12,
-    },
-});
