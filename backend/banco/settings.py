cb427b8b611770c17744262bfa415a6c71aabe80
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-05-29 16:22:04 -0300

    adicionando a url da vercel

--- backend/banco/settings.py
+++ backend/banco/settings.py
@@ -46,10 +46,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5173',
-    'http://localhost:5173',
-    'http://localhost:19006',
-    'exp://192.168.155.38:19000'
+    'exp://192.168.155.38:19000',
+    'https://fastbank.vercel.app'
 ]
 
 CORS_ALLOWED_ALL_ORIGINS = True
