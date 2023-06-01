a9dd0185ec1791824fa120f9b269d7ae421bdee9
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-06-01 16:44:35 -0300

    adicionando o localhost do mobile

--- backend/banco/settings.py
+++ backend/banco/settings.py
@@ -51,7 +51,9 @@ CORS_ALLOWED_ORIGINS = [
     'https://fastbank.vercel.app',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:8000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://localhost:19006'
+
 ]
 
 CORS_ALLOWED_ALL_ORIGINS = True
